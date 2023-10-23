8e47967e76c712e58e30c35d1daec5608ab1488a
haikali3 <105930061+haikali3@users.noreply.github.com>
2023-10-24 01:20:44 +0800

    modal wrapup

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import { useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
-function Modal(props) {
-  return (
-    <div className="modal">
-      <div className="modal-content">
-        <span className="close" onClick={props.onClose}>
-          &times; modal
-        </span>
-        {props.children}
+function Modal({ onClose, children, actionBar, title }) {
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden');
+
+    return () => {
+      document.body.classList.remove('overflow-hidden');
+    };
+  }, []);
+
+  return ReactDOM.createPortal(
+    <div>
+      <div
+        onClick={onClose}
+        className="fixed inset-0 bg-gray-300 opacity-80"
+      ></div>
+      <div className="fixed inset-40 p-10 bg-white">
+        <div className="flex flex-col justify-between h-full">
+          {children}
+          <div className="flex justify-end">{actionBar}</div>
+        </div>
       </div>
-    </div>
+    </div>,
+    document.querySelector('.modal-container')
   );
 }
 
